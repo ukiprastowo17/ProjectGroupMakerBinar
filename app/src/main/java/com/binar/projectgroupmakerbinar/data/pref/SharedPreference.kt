022b6b8c5381d6bbe7146a8013d055839193785d
ukiprastowo <ukiprastowo17@gmail.com>
2022-11-04 09:35:58 +0700

    init save key-value pref

---
+++ app/src/main/java/com/binar/projectgroupmakerbinar/data/pref/SharedPreference.kt
@@ -0,0 +1,40 @@
+package com.catnip.notepadku.data.pref
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+class SharedPreference (context: Context) {
+
+    private val preference : SharedPreferences = context.getSharedPreferences(NAME, Context.MODE_PRIVATE)
+
+    companion object{
+        private const val  NAME = "groupmaker_pref"
+    }
+
+
+    fun isSkipIntro(): Boolean {
+        return preference.getBoolean(
+            PreferenceKey.PREF_IS_SKIP_INTRO.first,
+            PreferenceKey.PREF_IS_SKIP_INTRO.second
+        )
+    }
+
+    fun setSkipIntro(isSkipIntro : Boolean){
+        preference.edit {
+            this.putBoolean(PreferenceKey.PREF_IS_SKIP_INTRO.first, isSkipIntro)
+        }
+    }
+
+    fun getToken(): String {
+        return preference.getString(
+            PreferenceKey.PREF_TOKEN.first,
+            PreferenceKey.PREF_TOKEN.second
+        )!!
+    }
+}
+
+object PreferenceKey{
+    val PREF_IS_SKIP_INTRO = Pair ("PREF_IS_SKIP_INTRO", false)
+    val PREF_TOKEN = Pair ("PREF_TOKEN", null)
+}
