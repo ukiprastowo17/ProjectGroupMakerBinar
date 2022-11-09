feac2bdbef76160b3d55f3b364941aff6588e4ca
ukiprastowo <ukiprastowo17@gmail.com>
2022-11-09 13:53:55 +0700

    struktur mvvm v2

---
+++ app/src/main/java/com/binar/projectgroupmakerbinar/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.binar.projectgroupmakerbinar.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+open class BaseActivity<B : ViewBinding>(
+    val bindingFactory: (LayoutInflater) -> B
+) : AppCompatActivity() {
+
+    protected val binding: B by lazy { bindingFactory(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+}
---
+++ app/src/main/java/com/binar/projectgroupmakerbinar/base/GenericViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.binar.projectgroupmakerbinar.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalArgumentException
+
+
+class GenericViewModelFactory(private val viewModel: ViewModel) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(viewModel::class.java)){
+            return viewModel as T
+        }
+        throw IllegalArgumentException("Unknown Class Name")
+    }
+
+}
