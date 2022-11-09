fbc40b3b7f83211209fdfd5ca1d1356343289526
ukiprastowo <ukiprastowo17@gmail.com>
2022-11-09 18:02:29 +0700

    merge  revisi bug 2

--- app/src/main/java/com/binar/projectgroupmakerbinar/data/room/dao/GroupDao.kt
+++ app/src/main/java/com/binar/projectgroupmakerbinar/data/room/dao/GroupDao.kt
@@ -9,7 +9,7 @@ import com.binar.projectgroupmakerbinar.data.room.entity.Group
 @Dao
 interface GroupDao {
 
-    @Query("SELECT * FROM " + CommonConstant.DATABASE_TABLE_GROUP)
+    @Query("SELECT * FROM " + CommonConstant.DATABASE_TABLE_GROUP + " ORDER BY " + CommonConstant.KEY_ROWID + " DESC")
     suspend fun getAllGroup() : List<Group>
 
 
